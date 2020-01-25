95ca43dda9c5f2fe9687ecffa188c70b22dadccd
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-01-25 16:36:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<form onsubmit="go()">
 		<p id="sign up" >Sign up<br></p>
 		<input id="name1.1" name="name1" placeholder="NAME"><br>
-		<input id="password1.1" name="password1" placeholder="PASSWORD"><br>
+		<input type="password" id="password1.1" name="password1" placeholder="PASSWORD"><br>
 		<input type="submit">
 		<input type="reset">
 		</form>
@@ -17,7 +17,7 @@ function go(){
 	y=document.getElementById("password1.1").value
 	document.open()
 	document.close()
-	document.write('<body background-color="lightred"><p id="sign in">Sign in<br></p><form action="signin.php" id="form2" method="post"><input id="name2" name="name2" placeholder="NAME"><br><input id="password2" name="password2" placeholder="PASSWORD"><input id="name1" name="name1" value="" hidden=true><br><input id="password1" name="password1" value="" hidden=true><br><input id="submit2" type="submit"><input id="reset2" type="reset"></form>')
+	document.write('<body background-color="lightred"><p id="sign in">Sign in<br></p><form action="signin.php" id="form2" method="post"><input id="name2" name="name2" placeholder="NAME"><br><input type="password" id="password2" name="password2" placeholder="PASSWORD"><input id="name1" name="name1" value="" hidden=true><br><input id="password1" name="password1" value="" hidden=true><br><input id="submit2" type="submit"><input id="reset2" type="reset"></form>')
 	document.getElementById("name1").value=x
 	document.getElementById("password1").value=y
 }
