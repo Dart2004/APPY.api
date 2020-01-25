5360cdce492f247083ad540e0735db88681bc872
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-01-25 16:33:13 +0100

    Update signin.php

--- signin.php
+++ signin.php
@@ -5,7 +5,7 @@ $p=$_POST["password1"];
 if ($_POST["name2"]==$l && $_POST["password2"]==$p)
 	echo "Login Sucessfull";
 else
-	echo 'Permission denied<br><form action="signin2.php" method="post">
+	echo '<body background-color="lightred">Permission denied<br><form action="signin2.php" method="post">
 <input onsubmit="go()" type="submit" value="Try again">
 <input name="name" hidden=true id="name" value="'.$l.'">
 <input name="password" hidden=true id="password" value="'.$p.'"></form>';
