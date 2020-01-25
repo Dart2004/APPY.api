5db5f043d441ebae95eaafc9729d33a1f4efb007
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-01-25 16:23:12 +0100

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,12 @@
+<p id="p"></p>
+<?php
+$l=$_POST["name1"];
+$p=$_POST["password1"];
+if ($_POST["name2"]==$l && $_POST["password2"]==$p)
+	echo "Login Sucessfull";
+else
+	echo 'Permission denied<br><form action="signin2.php" method="post">
+<input onsubmit="go()" type="submit" value="Try again">
+<input name="name" hidden=true id="name" value="'.$l.'">
+<input name="password" hidden=true id="password" value="'.$p.'"></form>';
+?>
